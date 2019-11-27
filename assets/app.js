9076e87bedd1f7974f711a8060394854c2ed250a
Paddlepop25 <shecodeshsucodes18@gmail.com>
2019-11-27 19:06:23 +0800

    deleted 3 files used for testing

--- assets/app.js
+++ assets/app.js
@@ -1,62 +1,108 @@
-// var map;
-// function initMap() {
-//   let singapore = { lat: 1.29027, lng: 103.851959 };
-//   map = new google.maps.Map(document.getElementById("map"), {
-//     center: singapore,
-//     zoom: 12
-//   });
-
-//   let marker = new google.maps.Marker({ position: singapore, map: map });
-// }
-
-// var map;
-
-// function initMap() {
-//   let kml1 = { lat: 1.3503086435268, lng: 103.952109671193 };
-//   map = new google.maps.Map(document.getElementById("map"), {
-//     center: kml1,
-//     zoom: 12
-//   });
-
-//   let marker = new google.maps.Marker({ position: kml1, map: map });
-// }
-
-var map;
-
 function initMap() {
-  let singapore = { lat: 1.29027, lng: 103.851959 };
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: singapore,
-    zoom: 11
+  let singapore = { lat: 1.35027, lng: 103.851959 };
+  let map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 11,
+    center: singapore
+  });
+  // var marker = new google.maps.Marker({ position: singapore, map: map });
+}
+
+let northEastArea = document
+  .querySelector(".north-east")
+  .addEventListener("click", () => {
+    northeastMap();
   });
 
-  let destinations = new google.maps.MVCArray();
-  destinations.push(new google.maps.LatLng(103.952109671193, 1.3503086435268));
-  destinations.push(
-    new google.maps.LatLng(103.952024903975996, 1.35027882272273)
-  );
-  destinations.push(
-    new google.maps.LatLng(103.951821994379003, 1.35079625373263)
-  );
-  destinations.push(
-    new google.maps.LatLng(103.951777809174004, 1.35090250311823)
-  );
-  destinations.push(
-    new google.maps.LatLng(103.951771863185996, 1.35091680228515)
-  );
-  destinations.push(
-    new google.maps.LatLng(103.951671200974005, 1.35115885803187)
-  );
-  destinations.push(new google.maps.LatLng(103.952109671193, 1.3503086435268));
+let northEastSelections = document
+  .querySelector(".northeastclinics")
+  .addEventListener("click", () => {
+    getNorthClinics();
+  });
 
-  let polylineOptions = {
-    path: destinations,
-    strokeColor: "ff0000",
-    strokeWeight: 5
+// let southEastArea = document
+//   .querySelector(".south-east")
+//   .addEventListener("click", () => {
+//     console.log("south-east");
+//   });
+
+// let centralArea = document
+//   .querySelector(".central")
+//   .addEventListener("click", () => {
+//     console.log("central");
+//   });
+
+// let southWestArea = document
+//   .querySelector(".south-west")
+//   .addEventListener("click", () => {
+//     console.log("south-west");
+//   });
+
+let resetButton = document
+  .querySelector(".reset-btn")
+  .addEventListener("click", () => {
+    initMap();
+  });
+
+function northeastMap() {
+  let northEast = { lat: 1.4326, lng: 103.8267 };
+  let map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 13,
+    center: northEast
+  });
+  // var marker = new google.maps.Marker({ position: northEast, map: map });
+}
+
+function getNorthClinics() {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open(
+    "GET",
+    "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=1.4326,103.8267&radius=5000&type=doctor&key=AIzaSyAQOzXrUwtwRVkzSyWzeRdxfpiPe7kBliU",
+    true
+  );
+
+  xhr.onload = function() {
+    if (this.status === 200) {
+      const response = JSON.parse(this.responseText);
+
+      loadClinicMarkers(response);
+    }
   };
 
-  let polyline = new google.maps.Polyline(polylineOptions);
-  polyline.setMap(map);
+  xhr.send();
+}
 
-  let marker = new google.maps.Marker({ position: singapore, map: map });
+function loadClinicMarkers(googleData) {
+  let northEast = { lat: 1.4326, lng: 103.8267 };
+  let map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 13,
+    center: northEast
+  });
+  // let result = Object.entries(googleData)[2][1][0]["geometry"]["location"];
+  let googleObject = Object.entries(googleData)[2][1];
+  for (let i = 0; i < googleObject.length; i++) {
+    // console.log(results[i]["geometry"]["location"]) // get 20 locations;
+    let location = googleObject[i]["geometry"]["location"];
+
+    addMarkers(map, location);
+  }
+
+  function addMarkers(map, latlong) {
+    let marker = new google.maps.Marker({
+      position: latlong,
+      map: map,
+      icon: "https://maps.gstatic.com/mapfiles/place_api/icons/doctor-71.png"
+    });
+  }
+
+  // let map = new google.maps.Map(document.getElementById("map"), {
+  //   zoom: 13,
+  //   center: result
+  // });
+
+  // let marker = new google.maps.Marker({
+  //   position: result,
+  //   map: map,
+  //   icon: "https://maps.gstatic.com/mapfiles/place_api/icons/doctor-71.png"
+  // });
 }
