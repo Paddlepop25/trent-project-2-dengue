f1d67e2fd85256161c643c11d29e2e47e18374b1
Paddlepop25 <shecodeshsucodes18@gmail.com>
2019-12-05 20:12:03 +0800

    designed ammenities card

---
+++ assets/east/eastshoppingmalls.js
@@ -0,0 +1,38 @@
+let eastSupermarkets = document
+  .querySelector(".east-shopping")
+  .addEventListener("click", () => {
+    geteastShoppingMalls();
+  });
+
+function geteastShoppingMalls() {
+  const xhr = new XMLHttpRequest();
+  xhr.open(
+    "GET",
+    "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=1.3497222222,103.9544444444&radius=4000&type=shopping_mall&key=AIzaSyAQOzXrUwtwRVkzSyWzeRdxfpiPe7kBliU",
+    true
+  );
+  xhr.onload = function() {
+    if (this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      loadEastShoppingMallMarkers(response);
+    } else {
+      alert(
+        "I'm sorry, there are too many requests. \nPlease try again in a second."
+      );
+    }
+  };
+  xhr.send();
+}
+
+function loadEastShoppingMallMarkers(googleData) {
+  let northEast = { lat: 1.3497222222, lng: 103.9544444444 };
+  let map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 13,
+    center: northEast
+  });
+  let googleObject = Object.entries(googleData)[2][1];
+  for (let i = 0; i < googleObject.length; i++) {
+    let listOfObjects = googleObject[i];
+    addMarkers(map, listOfObjects);
+  }
+}
