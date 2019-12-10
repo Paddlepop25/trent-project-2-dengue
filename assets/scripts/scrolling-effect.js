69c5cb31ff89a31a7882a85558185bcc619ea60d
Paddlepop25 <shecodeshsucodes18@gmail.com>
2019-12-10 17:33:25 +0800

    test without scrolling

--- assets/scripts/scrolling-effect.js
+++ assets/scripts/scrolling-effect.js
@@ -1,14 +1,14 @@
-let anchorlinks = document.querySelectorAll('a[href^="#"]')
+// let anchorlinks = document.querySelectorAll('a[href^="#"]')
 
-for (let item of anchorlinks) {
-  item.addEventListener('click', (e) => {
-    let hashval = item.getAttribute('href')
-    let target = document.querySelector(hashval)
-    target.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    })
-    history.pushState(null, null, hashval)
-    e.preventDefault()
-  })
-}
+// for (let item of anchorlinks) {
+//   item.addEventListener('click', (e) => {
+//     let hashval = item.getAttribute('href')
+//     let target = document.querySelector(hashval)
+//     target.scrollIntoView({
+//       behavior: 'smooth',
+//       block: 'start'
+//     })
+//     history.pushState(null, null, hashval)
+//     e.preventDefault()
+//   })
+// }
